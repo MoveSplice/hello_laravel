88d9c965989a8e0cb7e3bb4b6d5c10e92f126be2
326416055@qq.com <326416055@qq.com>
2018-07-24 19:30:52 +0800

    Say something

---
+++ sample/resources/lang/en/Notes.php
@@ -0,0 +1,64 @@
+﻿<?php
+
+class Database extends PDO{
+	
+	protected $transactionCount = 0;
+
+	public function beginTransaction(){
+
+		if(!$this -> $transactionCount++){
+
+			return parent::beginTransaction();
+
+		}
+
+		$this -> exec('SAVEPOINT trans'.$this->transactionCounter);
+
+		return $this->transactionCounter >=0;
+
+	}
+
+	public function commit(){
+
+		if(!--$this->transactionCounter){
+
+			return parent::comtent
+
+		}
+
+		$this->transactionCounter>=0;
+
+	}
+	
+	public function rollback(){
+	
+		if(!-$this->transactionCounter){
+
+			$this->exec('ROLLBACK TO trans'.$this->transactionCounter+1);
+			
+			return true;
+		}
+
+		return parent::rollback();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+?>
